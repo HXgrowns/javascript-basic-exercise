687decc2e795be8eed798eaa4a49021f66f1407b
HXgrowns <804156017@qq.com>
2020-07-21 23:31:24 +0800

    [huxiao]{modified}: modified exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,22 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array == undefined || array == null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length == 0) {
+    throw new Error('Creating list from empty array');
+  }
+  let list = {}
+  list.value = array[0];
+  list.next = null;
 
-  throw new Error('Please delete this line and implement the function');
+  let point = list;
+  for (let i = 1; i < array.length; i++) {
+    point.next = {};
+    point.next.value = array[i];
+    point.next.next = null;
+    point = point.next;
+  }
+  return list;
 }
